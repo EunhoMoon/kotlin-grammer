e282fff1e00aa786d3d384e355009be8a3d0748a
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-08-18 16:32:55 +0900

    Late Init

---
+++ src/main/kotlin/advanced/7_LateInit.kt
@@ -0,0 +1,27 @@
+package advanced
+
+/*
+  LateInit
+    - 가변 프로퍼티에 대한 지연 초기화를 위해 사용
+    - 가변 타입의 non null 프로퍼티에 사용
+    - DI, Junit setup 등 사용
+    - 초기화 전에 사용할 경우 Exception 발생
+    - isInitialized는 외부에서 접근할 수 없기 때문에 getter와 같은 방법으로 우회 접근해야 한다.
+*/
+class `7_LateExample` {
+
+    lateinit var text: String
+
+    fun printText() {
+        if (this::text.isInitialized) println("init")
+        else text = "Hello"
+
+        println(text)
+    }
+
+}
+
+fun main() {
+    val test = `7_LateExample`()
+    test.printText()
+}
