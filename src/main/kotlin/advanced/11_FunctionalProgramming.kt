335fbe7f700c451072e09cbdb5ae4ee9b46932a4
Janek <eunho.moon@gmail.com>
2023-08-21 21:46:01 +0900

    FP

--- src/main/kotlin/advanced/11_FunctionalProgramming.kt
+++ src/main/kotlin/advanced/11_FunctionalProgramming.kt
@@ -19,6 +19,29 @@ fun main() {
 
     val result: Int = plus(1, 2)
     println(result)
+
+    val strList = listOf("a", "b", "c")
+    val printStr: (String) -> Unit = { println(it) }
+    forEachStr(strList, printStr)
+
+    // 익명함수 호출
+    outerFunc()()
+
+    // 후행 람다 전달
+    forEachStr(strList) {
+        println(it)
+    }
+
+    // 람다 레퍼런스: 탑 레벨, 지역 변수일 경우 ::메서드, 객체의 멤버일 경우 객체::메서드 로 실행
+//    val callReference: () -> Unit = { printHello() }
+    val callReference = ::printHello
+
+    callReference()()
+
+    val numberList = listOf("1", "2", "3")
+
+//    numberList.map { it.toInt() }.forEach { println(it) }
+    numberList.map(String::toInt).forEach(::println)
 }
 
 //fun printHello() {
@@ -38,3 +61,18 @@ fun printNo() = println("No")
 val printMessage: (String) -> Unit = { println(it) }    // it을 통해 인자에 대한 참조 표현 가능
 
 val plus: (Int, Int) -> Int = { a, b -> a + b }
+
+// 고차함수: 함수를 인자로 받아 처리하거나 결과를 반환하는 함수, forEach, map도 고차함수이다.
+fun forEachStr(collection: Collection<String>, action: (String) -> Unit) {
+    for (item in collection) {
+        action(item)
+    }
+}
+
+// 익명함수: 이름 없이 함수를 실행하고 해당 결과 값을 반환하는 함수, 람다로 변경해서 사용할 수 있다.
+//fun outerFunc() : () -> Unit {
+//    return fun() {
+//        println("익명함수 호출")
+//    }
+//}
+fun outerFunc() : () -> Unit = { println("익명함수 호출") }
