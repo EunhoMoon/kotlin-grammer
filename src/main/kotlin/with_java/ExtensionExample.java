962c673e8679d1290958d95c2559774f7e52f908
Janek <eunho.moon@gmail.com>
2023-08-27 21:45:19 +0900

    with java - extension method

--- src/main/kotlin/with_java/ExtensionExample.java
+++ src/main/kotlin/with_java/ExtensionExample.java
@@ -1,6 +1,17 @@
 package with_java;
 
 public class ExtensionExample {
-//    "ABCD".first();
-//    "ABCD".addFirst('Z');
+
+    public static void main(String[] args) {
+        // "ABCD".first();
+        // "ABCD".addFirst('Z');
+
+        // 확장함수를 해당 파일명 + Kt의 static 메서드 형태로 호출해야 한다.
+        char first = MyExtensionsKt.first("ABCD");
+        String addFirst = MyExtensionsKt.addFirst("ABCD", 'Z');
+
+        System.out.println("first = " + first);
+        System.out.println("addFirst = " + addFirst);
+    }
+
 }
